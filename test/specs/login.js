79e3c32b731b9f4f4e0f67baf8c08052714407bf
indirapriyadasrhini05 <indira.priyadarshini@excellarate.com>
2022-11-07 18:23:04 +0530

    added

--- test/specs/login.js
+++ test/specs/login.js
@@ -70,6 +70,7 @@ credentials.forEach(({ username, password, invusername, invpassword }) => {
       await navBar.leftNav_Admin();
       await logout_Fn.logout();
       console.log('hi');
+      console.log('hi');
     });
 
     it.only("Login with inValid credentials to OrangeHRM", async () => {
